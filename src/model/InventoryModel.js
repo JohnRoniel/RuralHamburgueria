e4b21ad94c1f1e9a5ecf18290906bb805ed79c3b
JRoniel <jroniel055@gmail.com>
2025-01-27 15:40:52 -0300

    :construction: Added resource inventory this is project

---
+++ src/model/InventoryModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+// Definindo o esquema para o Inventário
+const inventorySchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.SchemaTypes.ObjectId,
+        auto: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    control: {
+        type: Boolean,
+        default: true,
+    },
+    lastUpdated: {
+        type: String
+    },
+});
+
+// Função para formatar a data no formato "00:00 01/01/2025"
+function formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${day}/${month}/${year} às ${hours}:${minutes}`;
+}
+
+// Hook para atualizar a data da última alteração
+inventorySchema.pre('save', function (next) {
+    const currentDate = new Date();
+    this.lastUpdated = formatDate(currentDate); // Atualiza o campo lastUpdated com a data formatada
+    next();
+});
+
+
+// Criando o modelo do Inventário
+const Inventory = mongoose.model('Inventory', inventorySchema);
+
+module.exports = Inventory;
--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getDb } = require('../config/database');
-const { ObjectId } = require('mongodb');
+const Inventory = require('../model/inventoryModel');
 
 router.get('/', async (req, res) => {
   try {
-    const db = getDb();
-    const products = await db.collection('products')
-      .aggregate([
-        {
-          $lookup: {
-            from: 'categories',
-            localField: 'category_id',
-            foreignField: '_id',
-            as: 'category'
-          }
-        },
-        {
-          $unwind: {
-            path: '$category',
-            preserveNullAndEmptyArrays: true
-          }
-        }
-      ]).toArray();
+    const inventory = await Inventory.find();
 
-    res.render('products/index', {
-      products,
+    res.render('inventory/index', {
+      inventory,
       title: 'Produtos',
       layout: 'layout'
     });
@@ -36,10 +18,9 @@ router.get('/', async (req, res) => {
 
 router.get('/new', async (req, res) => {
   try {
-    const db = getDb();
-    const categories = await db.collection('categories').find().toArray();
-    res.render('products/new', { 
-      categories,
+    const inventory = await Inventory.find();
+    res.render('inventory/new', { 
+      inventory,
       title: 'Novo Produto',
       layout: 'layout'
     });
@@ -50,22 +31,34 @@ router.get('/new', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const db = getDb();
-    const { name, description, price, quantity, category_id } = req.body;
-    await db.collection('products').insertOne({
+    const { name, category, count, control} = req.body;
+    const newInventory = new Inventory({
       name,
-      description,
-      price: parseFloat(price),
-      quantity: parseInt(quantity),
-      category_id: new ObjectId(category_id),
-      created_at: new Date(),
-      updated_at: new Date()
+      category,
+      count,
+      control
     });
+    await newInventory.save();
 
-    res.redirect('/products');
+    res.redirect('/inventory');
+  } catch (error) {
+    res.status(500).render('error', { error, layout: 'layout' });
+  }});
+
+router.post('/getItems', async (req, res) => {
+  try {
+    const { category } = req.body;
+    const inventory = await Inventory.findOne({ category });
+    
+    console.log(inventory);
+
+    res.render('inventory/index', {
+      inventory,
+      title: 'Produtos retornados',
+      layout: 'layout'
+    });
   } catch (error) {
     res.status(500).render('error', { error, layout: 'layout' });
   }
 });
-
 module.exports = router;
