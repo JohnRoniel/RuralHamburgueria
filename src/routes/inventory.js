5fb5575e3a454cb3454348a97e0460e3a89dd36b
JRoniel <jroniel055@gmail.com>
2025-01-27 17:41:10 -0300

    New routes for EDIT, DELETE and SAVE

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -29,36 +29,86 @@ router.get('/new', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.get('/edit/:id', async (req, res) => {
   try {
-    const { name, category, count, control} = req.body;
-    const newInventory = new Inventory({
-      name,
-      category,
-      count,
-      control
-    });
-    await newInventory.save();
-
-    res.redirect('/inventory');
-  } catch (error) {
-    res.status(500).render('error', { error, layout: 'layout' });
-  }});
-
-router.post('/getItems', async (req, res) => {
-  try {
-    const { category } = req.body;
-    const inventory = await Inventory.findOne({ category });
+    const { id } = req.params;
+    const inventory = await Inventory.findOne({ _id: id });
     
-    console.log(inventory);
-
-    res.render('inventory/index', {
+    res.render('inventory/edit', {
       inventory,
-      title: 'Produtos retornados',
       layout: 'layout'
     });
   } catch (error) {
     res.status(500).render('error', { error, layout: 'layout' });
   }
 });
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, category, count, control, countMin } = req.body;
+
+    const newInventory = new Inventory({
+      name,
+      countMin,
+      category,
+      count,
+      control
+    });
+
+    await newInventory.save();
+    res.redirect('/inventory');
+  } catch (error) {
+    res.status(500).render('error', { error, layout: 'layout' });
+  }
+});
+
+  router.post('/getItems', async (req, res) => {
+    try {
+      const { category } = req.body;
+      const inventory = await Inventory.find({ category }); // alterado para find, retornando múltiplos itens
+  
+      res.render('inventory/index', {
+        inventory,
+        title: 'Produtos retornados',
+        layout: 'layout'
+      });
+    } catch (error) {
+      res.status(500).render('error', { error, layout: 'layout' });
+    }
+  });
+  
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Inventory.deleteOne({ _id: id });
+
+    res.redirect('/inventory');
+  } catch (error) {
+    res.status(500).render('error', { error, layout: 'layout' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, category, count, control, countMin } = req.body;
+
+    // Usando findByIdAndUpdate para atualizar ou retornar erro caso não encontre
+    const updatedItem = await Inventory.findByIdAndUpdate(
+      id,
+      { name, category, count, control, countMin },
+      { new: true, runValidators: true } // `new: true` retorna o documento atualizado
+    );
+
+    if (!updatedItem) {
+      return res.status(404).render('error', { error: 'Item não encontrado', layout: 'layout' });
+    }
+
+    res.redirect('/inventory');
+  } catch (error) {
+    res.status(500).render('error', { error, layout: 'layout' });
+  }
+});
+
 module.exports = router;
